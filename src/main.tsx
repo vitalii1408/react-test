70bd87af6d3876ac95201de5cc78caee3dc2fc87
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-09-09 07:34:42 +0300

    5

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 // main.tsx
 
+import { createRoot } from "react-dom/client";
 
 export default function App() {
-  const handleSubmit = (formData: FormData) => {
-	  console.log("Form submitted");
+  const handleSubmit = (_formData: FormData) => {
+    console.log("Form submitted");
   };
 
   return (
-		<form action={handleSubmit}>
+    <form action={handleSubmit}>
       <input type="text" name="username" />
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+createRoot(document.getElementById("root")!).render(<App />);
 
