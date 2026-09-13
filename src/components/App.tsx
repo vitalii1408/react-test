8beabb2b4a701471b8bfecd31ea013d59768a4fe
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-09-13 20:20:21 +0300

    8

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const fetchPerson = async () => {
-  const response = await axios.get(`https://swapi.info/api/people/1`);
+const fetchPerson = async (id: number) => {
+  const response = await axios.get(`https://swapi.info/api/people/${id}`);
   return response.data;
 };
 
 export default function App() {
+  const [count, setCount] = useState(1);
+
   const { data, error, isLoading, isError } = useQuery({
-    queryKey: ['person'],
-    queryFn: fetchPerson,
+    queryKey: ['person', count],  // змінюємо ключ запиту залежно від count
+    queryFn: () => fetchPerson(count),
   });
 
   return (
     <>
+      <button onClick={() => setCount(count + 1)}>Get next character</button>
       {isLoading && <p>Loading...</p>}
-      {isError && <p>An error occurred: {error.message}</p>}
+      {isError && <p>Error: {error?.message}</p>}
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </>
   );
